9cd05a89b82f8e932839f4c0eef5b64dbc90008a
ar03324 <ar03324@georgiasouthern.edu>
2014-10-12 22:10:02 -0400

    added states and ADDITEM

--- StringConstants.java
+++ StringConstants.java
@@ -1,7 +1,13 @@
-//holds constants
-//these constants represent keys to specific panels
 public interface StringConstants {
 	final static String LOGIN = "Login Screen";
-	final static String CREATENEW = "Create new User";
+	final static String CREATENEW = "Create New User";
 	final static String CONN = "Connection Success Display"; 
+	final static String ADDITEM = "Add Item";
+	final static String[] StatesList = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+		"Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+		"Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massacusetts", "Michigan", "Minnesota", "Mississippi",
+		"Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+		"North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", 
+		"South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+		"West Virginia", "Wisconsin", "Wyoming"};
 }
