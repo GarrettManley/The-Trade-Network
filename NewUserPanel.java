d72a6840dc2566fe7b4b93e02688f57888de36a1
ar03324 <ar03324@georgiasouthern.edu>
2014-10-12 21:54:58 -0400

    "Functional" New User Panel
    Functional design anyway

--- NewUserPanel.java
+++ NewUserPanel.java
@@ -5,6 +5,13 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.TextField;
+import java.awt.TextArea;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JComboBox;
+import java.awt.Font;
 
 /*How to create a new user for a database in sql
  * Example:
@@ -19,10 +26,15 @@ GO
 ***********************************************/
 
 public class NewUserPanel extends JPanel implements StringConstants{
-	private JTextField textField;
 	private JPanel mainpanel;
 	private JButton goBack;
 	private DBConnection db;
+	private JTextField txtUsername;
+	private JPasswordField pwdPassword;
+	private JPasswordField passwordField;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Create the panel.
@@ -33,14 +45,74 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		this.db = db;
 		
 		goBack = new JButton("GO BACK");
-		goBack.setBounds(181, 221, 89, 23);
+		goBack.setBounds(234, 231, 89, 23);
 		add(goBack);
-		goBack.addActionListener(new NewUserPanelListener());
+		
+		txtUsername = new JTextField();
+		txtUsername.setBounds(234, 83, 146, 20);
+		add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		JLabel lblCreateNewUser = new JLabel("Create New User");
+		lblCreateNewUser.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblCreateNewUser.setBounds(10, 11, 243, 23);
+		add(lblCreateNewUser);
+		
+		pwdPassword = new JPasswordField();
+		pwdPassword.setBounds(234, 129, 146, 20);
+		add(pwdPassword);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(234, 114, 146, 14);
+		add(lblPassword);
+		
+		JLabel lblConfirmPassword = new JLabel("Confirm Password");
+		lblConfirmPassword.setBounds(234, 163, 146, 14);
+		add(lblConfirmPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(234, 178, 146, 20);
+		add(passwordField);
+		
+		JLabel lblEmailAddress = new JLabel("Email Address");
+		lblEmailAddress.setBounds(234, 69, 146, 14);
+		add(lblEmailAddress);
+		
+		JLabel lblStreetAddressLine = new JLabel("Street Address Line 1");
+		lblStreetAddressLine.setBounds(10, 69, 146, 14);
+		add(lblStreetAddressLine);
 		
 		textField = new JTextField();
-		textField.setBounds(25, 26, 389, 189);
+		textField.setBounds(10, 83, 146, 20);
 		add(textField);
 		textField.setColumns(10);
+		
+		JLabel lblStreetAddressLine_1 = new JLabel("Street Address Line 2");
+		lblStreetAddressLine_1.setBounds(10, 114, 146, 14);
+		add(lblStreetAddressLine_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(10, 129, 146, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblCity = new JLabel("City");
+		lblCity.setBounds(10, 163, 146, 14);
+		add(lblCity);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(10, 178, 146, 20);
+		add(textField_2);
+		textField_2.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox(StatesList);
+		comboBox.setBounds(10, 231, 146, 20);
+		add(comboBox);
+		
+		JLabel lblState = new JLabel("State");
+		lblState.setBounds(10, 217, 46, 14);
+		add(lblState);
+		goBack.addActionListener(new NewUserPanelListener());
 
 	}
 	
