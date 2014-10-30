5e3fd47f3f71f046f0aad9b33bef027c91aca8f2
mkruzel <mkruzel@users.noreply.github.com>
2014-10-30 11:15:35 -0400

    Update NewUserPanel.java

--- NewUserPanel.java
+++ NewUserPanel.java
@@ -6,6 +6,8 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
+
+import java.awt.Dimension;
 import java.awt.TextField;
 import java.awt.TextArea;
 import javax.swing.JLabel;
@@ -13,20 +15,10 @@ import javax.swing.JPasswordField;
 import javax.swing.JComboBox;
 import java.awt.Font;
 
-/*How to create a new user for a database in sql
- * Example:
--- Creates the login AbolrousHazem with password '340$Uuxwp7Mcxo7Khy'.
-CREATE LOGIN AbolrousHazem 
-    WITH PASSWORD = '340$Uuxwp7Mcxo7Khy';
-GO
 
--- Creates a database user for the login created above.
-CREATE USER AbolrousHazem FOR LOGIN AbolrousHazem;
-GO
-***********************************************/
 
 public class NewUserPanel extends JPanel implements StringConstants{
-	private JPanel mainpanel;
+	private TheMainPanel mainpanel;
 	private JButton goBack;
 	private DBConnection db;
 	private JTextField txtUsername;
@@ -40,17 +32,19 @@ public class NewUserPanel extends JPanel implements StringConstants{
 	/**
 	 * Create the panel.
 	 */
-	public NewUserPanel(JPanel mainpanel, DBConnection db) {
+	public NewUserPanel(TheMainPanel mainpanel, DBConnection db) {
 		setLayout(null);
 		this.mainpanel = mainpanel;
 		this.db = db;
 		
+		setPreferredSize(new Dimension(450,500));
+		
 		goBack = new JButton("GO BACK");
-		goBack.setBounds(234, 266, 89, 23);
+		goBack.setBounds(97, 371, 89, 23);
 		add(goBack);
 		
 		txtUsername = new JTextField();
-		txtUsername.setBounds(234, 63, 146, 20);
+		txtUsername.setBounds(234, 83, 188, 20);
 		add(txtUsername);
 		txtUsername.setColumns(10);
 		
@@ -60,23 +54,23 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		add(lblCreateNewUser);
 		
 		pwdPassword = new JPasswordField();
-		pwdPassword.setBounds(234, 111, 146, 20);
+		pwdPassword.setBounds(234, 132, 188, 20);
 		add(pwdPassword);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(234, 94, 146, 14);
+		lblPassword.setBounds(234, 114, 146, 14);
 		add(lblPassword);
 		
 		JLabel lblConfirmPassword = new JLabel("Confirm Password");
-		lblConfirmPassword.setBounds(234, 142, 146, 14);
+		lblConfirmPassword.setBounds(234, 163, 146, 14);
 		add(lblConfirmPassword);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(234, 160, 146, 20);
+		passwordField.setBounds(234, 178, 188, 20);
 		add(passwordField);
 		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(234, 45, 146, 14);
+		lblUsername.setBounds(234, 69, 146, 14);
 		add(lblUsername);
 		
 		JLabel lblStreetAddress = new JLabel("Street Address");
@@ -84,7 +78,7 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		add(lblStreetAddress);
 		
 		textField = new JTextField();
-		textField.setBounds(10, 83, 146, 20);
+		textField.setBounds(10, 83, 176, 20);
 		add(textField);
 		textField.setColumns(10);
 		
@@ -93,7 +87,7 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		add(lblState);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(10, 129, 146, 20);
+		textField_1.setBounds(10, 129, 176, 20);
 		add(textField_1);
 		textField_1.setColumns(10);
 		
@@ -102,12 +96,12 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		add(lblCity);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(10, 232, 146, 20);
+		textField_2.setBounds(10, 232, 176, 20);
 		add(textField_2);
 		textField_2.setColumns(10);
 		
 		JComboBox comboBox = new JComboBox(StatesList);
-		comboBox.setBounds(10, 178, 146, 20);
+		comboBox.setBounds(10, 178, 176, 20);
 		add(comboBox);
 		
 		JLabel lblZipCode = new JLabel("Zip Code");
@@ -115,13 +109,17 @@ public class NewUserPanel extends JPanel implements StringConstants{
 		add(lblZipCode);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(234, 232, 146, 20);
+		textField_3.setBounds(234, 232, 188, 20);
 		add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblPhoneNumber = new JLabel("Phone Number");
 		lblPhoneNumber.setBounds(234, 217, 146, 14);
 		add(lblPhoneNumber);
+		
+		JButton btnCreate = new JButton("CREATE");
+		btnCreate.setBounds(234, 371, 89, 23);
+		add(btnCreate);
 		goBack.addActionListener(new NewUserPanelListener());
 
 	}
@@ -132,6 +130,7 @@ public class NewUserPanel extends JPanel implements StringConstants{
 			if(e.getSource() == goBack){
 				CardLayout cl = (CardLayout)(mainpanel.getLayout());
 				cl.show(mainpanel, LOGIN);
+				mainpanel.changeSize(450,300);
 			}
 			
 		}
